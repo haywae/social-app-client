62a4a619a23d4aa3b636d87c4a3f0d4a589e4588
haywae <haywae35@gmail.com>
2025-10-13 22:41:27 +0300

    removed border from profile page and changed the tab link hover color

--- src/pages/createPostPage.tsx
+++ src/pages/createPostPage.tsx
@@ -4,6 +4,7 @@ import { parseTags } from "../utils/tagsUtils";
 import { useNavigate } from "react-router-dom";
 import { CloseIcon, EmojiIcon } from "../assets/icons";
 import { createPost  } from "../thunks/postsThunks/createPostThunk";
+import { fetchDiscoveryData } from "../thunks/searchThunks/fetchDiscoveryThunk";
 import { resetCreatePostStatus, setCreatePostContent } from "../slices/posts/postsSlice";
 import { setError } from "../slices/ui/uiSlice";
 import "../styles/createPostPage.css"
@@ -75,10 +76,10 @@ const CreatePostPage = () : JSX.Element | null => {
         // The thunk will handle success/failure and update the state
         try {
             await dispatch(createPost({ content: createPostContent, tags})).unwrap();
+            await dispatch(fetchDiscoveryData())
         } catch(err: any) {
             dispatch(setError(err))
         }
-
     };
 
     const handleCloseButton = () => {
